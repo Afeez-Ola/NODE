e7b0e5e715c05447ddd6126a119aa7c4489c697f
Afeez-Ola <afeezbolajiola@gmail.com>
2021-04-02 08:58:07 +0100

    🎨 IMPROVE:

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 // jshint esversion:6
+const { json } = require('body-parser');
 const express = require('express');
 const fs = require('fs');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type": "application/pdf" });
     // res.write('Hello World!')
-    fs.readFile('index.html', (err, data) => {
+
+    fs.readFile('MOFA SCHOLARSHIP SIGNED.pdf', (err, data) => {
         if (err)
             throw err;
-        console.log('operation success!');
+
+        res.end(data)
     })
-    res.end();
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`server currently live on PORT:${PORT}`)
+    console.log(`server currently live on PORT:${PORT}`);
 })
