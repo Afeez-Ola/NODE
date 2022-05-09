a30eab41e663fd38714a2965f3821608f0b361c4
Afeez Bolaji <afeezbolajiola@gmail.com>
2022-05-09 23:45:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,29 +22,3 @@ app.listen(PORT, () => {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server started on port:${PORT}`);
-});
