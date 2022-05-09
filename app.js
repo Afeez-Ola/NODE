df4d355b55d6cab472c2aa8eb41e8c689bd83d9d
Afeez Bolaji <afeezbolajiola@gmail.com>
2022-05-09 23:44:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 
 const app = express();
 
-const PORT = 3000
+const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is started on port ${PORT};
 }
